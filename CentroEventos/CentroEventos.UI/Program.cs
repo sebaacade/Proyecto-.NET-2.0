ae13a57db84a90542b78f3cda22ffa3d2d005f7b
Matias <caubetmatias@gmail.com>
2025-06-19 02:46:40 -0300

    corregida interfaz

--- CentroEventos/CentroEventos.UI/Program.cs
+++ CentroEventos/CentroEventos.UI/Program.cs
@@ -52,10 +52,10 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
 }
 
-
+app.UseStaticFiles();
 app.UseAntiforgery();
 
-//app.MapStaticAssets();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
 
